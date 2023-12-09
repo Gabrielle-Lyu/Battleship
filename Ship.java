8b027b6f83b211c748a6d28be5b0ee289f4f4933
sharonyu524 <xy3@wellesley.edu>
2023-12-09 14:24:58 -0500

    Implemented okToPlaceShipAt and placeShipAt

--- Ship.java
+++ Ship.java
@@ -13,8 +13,53 @@ public abstract class Ship {
     abstract String getShipType();
     boolean isHorizontal(){ return horizontal; }
     boolean isSunk(){ return false; }
-    boolean okToPlaceShipAt(int row, int column, boolean horizontal, Ocean ocean){ return false; }
-    void placeShipAt(int row, int column, boolean horizontal, Ocean ocean){ }
+    boolean okToPlaceShipAt(int row, int column, boolean horizontal, Ocean ocean){
+        if(horizontal){
+            if((column + this.length)>9){
+                return false;
+            }
+        } else{
+            if((row + this.length)>9){
+                return false;
+            }
+        }
+
+        for(int i = 0; i < this.length; i++){
+            int checkRow = horizontal ? row : row + i;
+            int checkColumn = horizontal ? column + i : column;
+
+            if (ocean.isOccupied(checkRow,checkColumn)){
+                return false;
+            }
+            for (int dr = -1; dr <= 1; dr++){
+                for(int dc = -1; dc <=1; dc++){
+                    int adjacentRow = checkRow + dr;
+                    int adjacentColumn = checkColumn + dc;
+
+                    if (adjacentRow >= 0 && adjacentRow < 10 &&
+                            adjacentColumn >= 0 && adjacentColumn < 10) {
+                        if (ocean.isOccupied(adjacentRow, adjacentColumn)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    void placeShipAt(int row, int column, boolean horizontal, Ocean ocean){
+        this.bowRow = row;
+        this.bowColumn = column;
+        this.horizontal = horizontal;
+
+        for (int i = 0; i < length; i++){
+            if (horizontal){
+                ocean.ships[row][column+i] = this;
+            } else{
+                ocean.ships[row+i][column] = this;
+            }
+        }
+    }
     void setBowColumn(int bowColumn){
         this.bowColumn = bowColumn;
     }
